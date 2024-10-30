047d450acd73a6f4481e331b03b865416e4443ca
Sidharthsidd <sidh22cs@cmrit.ac.in>
2024-10-30 17:55:28 +0530

    linkked list added

--- linkedlist.py
+++ linkedlist.py
@@ -36,6 +36,40 @@ class SLL:
         while temp is not None :
             print(temp.item,end=" ")
             temp=temp.next
+    def insert_after(self,data,temp):
+        if temp is not None:
+            n=Node(data,temp.next)
+            temp.next=n
+    def delete_first():
+        if self.start is not None :
+            self.start=self.start.next
+    def delete_last ():
+        if self.startis None :
+            pass
+        elif self.start.next is None:
+            self.start=None
+        else:
+            temp=self.start
+            while temp.next.next is not None:
+                temp=temp.next
+            temp.next=None
+        def delete_item(self,data):
+            temp=self.start
+            if self.start is None :
+                pass
+        elif self.start.next is None :
+            if self.start.item==data:
+                self.start=None
+        else:
+            temp=self.start
+            
+
+
+            
+
+
+
+        
 
 
 #driveing code 
